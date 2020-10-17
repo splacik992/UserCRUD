5dbeb5e2c13db9cd03803e05a9cb34adfc784b94
rafal <r.paliwoda992@gmail.com>
2020-10-17 11:08:26 +0200

    Context/DBUtil and User/UserDao added

---
+++ src/main/java/pl/utils/User.java
@@ -0,0 +1,52 @@
+package pl.utils;
+
+public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
